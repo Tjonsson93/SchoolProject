efbdab60d3a8cb012cacb0cb40de742061060cd4
Sarah <adinsarah@gmail.com>
2020-12-07 22:10:59 +0100

    Main java test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//package com.company;
+
+import express.Express;
+import express.middleware.Middleware;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Express app = new Express();
+        Database db = new Database();
+
+
+        try {
+            app.use(Middleware.statics(Paths.get("src/frontend").toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        app.listen(3000);
+        System.out.println("server started at port 3000");
+    }
+}
+
     }
 }
