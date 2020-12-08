83efcb58a07c98062a337f71f3c8bca81a53618b
Sarah <adinsarah@gmail.com>
2020-12-08 14:12:48 +0100

    js

--- src/Frontend/script.js
+++ src/Frontend/script.js
@@ -1,9 +1,26 @@
+function deleteFunction() {
+    let deleteButtons = $(".deleteButton");
+    deleteButtons.empty();
 
+    for (let i = 0; i < deleteButtons.length; i++) {
+        $(deleteButtons[i]).click(function () {
+            let parentElement = this.parentElement;
+            parentElement.style.display = "none";
+            deleteNote(note[i]);
+            notes.splice(i,1); 
+        })
+    }
+}
+async function deleteNote(note) {
 
-
-
-
-
+    let noteToDelete = {
+        id: notes.id,
+    }
+    let result = await fetch("/rest/items/id", {
+        method: "DELETE",
+        body: JSON.stringify(noteToDelete)
+    });
+}
 
 
 
