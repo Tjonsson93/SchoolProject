cf746e1c6945c39e6dd5b550993c6ed346d2d2c4
Tobias  Jönsson <tobbe-j93@hotmail.com>
2020-12-09 09:09:36 +0100

    add function html change , render functions

--- src/Frontend/script.js
+++ src/Frontend/script.js
@@ -1,3 +1,74 @@
+let notes = [];
+
+
+
+function addNote() {
+    let titleInput = $("#titleInput").val();
+    let textInput = $("#noteField").val();
+    
+    
+    if (titleInput.length > 0 && textInput.length > 0) {
+
+        let note = {
+            title: titleInput,
+            text: textInput
+        }
+
+        notes.push(note);
+       
+    } else {
+        alert("Please enter title and text.");
+    }
+
+    renderList();
+    renderTitleList();
+}
+
+
+
+function renderList() {
+    let list = $("#notesList");
+    list.empty();
+    
+     for (everyNote of notes) {
+        list.append(`<li> <h3>${everyNote.title}</h3> <br> <p>${everyNote.text}</p></li>`);
+    }
+    
+
+}
+
+
+function renderTitleList() {
+    let listOfTitles = $("#titleList");
+    listOfTitles.empty();
+    
+    for (everyTitle of notes) {
+        listOfTitles.append(`<li><h4> ${everyTitle.title} </h4> </li>`);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function deleteFunction() {
     let deleteButtons = $(".deleteButton");
     deleteButtons.empty();
